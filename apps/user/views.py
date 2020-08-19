0a9e3f81dce541b8ddafcbb2bd628da670c99b2a
Sarahxed <1027719545@qq.com.com>
2020-08-20 00:59:27 +0800

    新增注册用户名/手机号码验证,新增登录表单

--- apps/user/views.py
+++ apps/user/views.py
@@ -6,7 +6,7 @@
 @file  : views.py
 @time  : 2020/8/14 16:40
 """
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, jsonify, redirect, url_for
 from werkzeug.security import generate_password_hash
 
 from apps.user.models import User
@@ -17,11 +17,13 @@ user_bp = Blueprint('user', __name__, url_prefix='/user')
 
 @user_bp.route('/index', endpoint='index')
 def index():
-    return render_template('base.html')
+    """首页"""
+    return render_template('user/index.html')
 
 
 @user_bp.route('/register', methods=['POST', 'GET'], endpoint='register')
 def register():
+    """用户注册"""
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
@@ -36,5 +38,33 @@ def register():
             user.email = email
             db.session.add(user)
             db.session.commit()
-            return "注册成功！"
-    return render_template("user/register.html")
+            return redirect(url_for('user.index'))
+    return render_template('user/register.html')
+
+
+@user_bp.route('/isvalid_phone', methods=['POST', 'GET'], endpoint="isvalid_phone")
+def is_valid_phone():
+    """注册时手机号码校验"""
+    phone = request.args.get('phone')
+    user = User.query.filter(User.phone == phone).all()
+    if len(user) > 0:
+        return jsonify(code=400, msg="此号码已被注册")
+    else:
+        return jsonify(code=200, msg="此号码可用")
+
+
+@user_bp.route('/isvalid_username', methods=['POST', 'GET'], endpoint="isvalid_username")
+def is_valid_username():
+    """注册时用户名校验"""
+    username = request.args.get('username')
+    user = User.query.filter(User.username == username).all()
+    if len(user) > 0:
+        return jsonify(code=400, msg="用户名已被注册")
+    else:
+        return jsonify(code=200, msg="此用户名可用")
+
+
+@user_bp.route('/login', methods=['POST', 'GET'], endpoint="login")
+def login():
+    msg = "登录"
+    return render_template('user/login.html', msg=msg)
